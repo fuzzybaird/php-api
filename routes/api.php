ffe80dacb5e1df8d8826ca58226f359183fa5238
Eli Baird <eli.baird@chghealthcare.com>
2019-08-12 20:33:09 -0400

    adding new routes to php app

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,44 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->get('/health', function (Request $request) {
+    return ["status" => "healthy"];
+});
+
+Route::middleware('auth:api')->get('/providers', function (Request $request) {
+    return ["providers" => [
+            [
+                "name" => "John Doe",
+                "npi" => "12346355"
+            ],
+            [
+                "name" => "Jane Doe",
+                "npi" => "123463523"
+            ],
+            [
+                "name" => "Mic Doeser",
+                "npi" => "123451234"
+            ],
+            [
+                "name" => "CatMan Shulzy",
+                "npi" => "734562342"
+            ],
+            [
+                "name" => "McFarlin Catz",
+                "npi" => "123476355"
+            ],
+            [
+                "name" => "Dog Bog",
+                "npi" => "3463456345"
+            ],
+            [
+                "name" => "Cool Hand Luke",
+                "npi" => "234352345"
+            ],
+            [
+                "name" => "Bob The Biznessman",
+                "npi" => "73456463"
+            ],
+        ]
+    ];
 });
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,41 @@ Route::get('/', function () {
 Route::get('/health', function () {
     return ["status" => "healthy"];
 });
+
+Route::get('/providers', function () {
+    return ["providers" => [
+            [
+                "name" => "John Doe",
+                "npi" => "12346355"
+            ],
+            [
+                "name" => "Jane Doe",
+                "npi" => "123463523"
+            ],
+            [
+                "name" => "Mic Doeser",
+                "npi" => "123451234"
+            ],
+            [
+                "name" => "CatMan Shulzy",
+                "npi" => "734562342"
+            ],
+            [
+                "name" => "McFarlin Catz",
+                "npi" => "123476355"
+            ],
+            [
+                "name" => "Dog Bog",
+                "npi" => "3463456345"
+            ],
+            [
+                "name" => "Cool Hand Luke",
+                "npi" => "234352345"
+            ],
+            [
+                "name" => "Bob The Biznessman",
+                "npi" => "73456463"
+            ],
+        ]
+    ];
+});
